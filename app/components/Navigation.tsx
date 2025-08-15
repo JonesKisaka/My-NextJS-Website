2aeacc501d56944a0c4651d83fa92c1285308c4c
JonesKisaka <kisakathejones@gmail.com>
2025-08-15 15:47:07 +0300

    navbar modification

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -2,10 +2,8 @@ export default function Navigation() {
   return (
     <nav className="navigation">
       <div className="nav-container">
-        <div className="nav-brand">
-          <a href="#home" className="nav-link">Home</a>
-        </div>
         <div className="nav-menu">
+          <a href="#home" className="nav-link">Home</a>
           <a href="#about" className="nav-link">About</a>
         </div>
       </div>
