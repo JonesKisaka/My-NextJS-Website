a4a93fce2d6b9bdd83c29834e2dfde4b8fb5a161
JonesKisaka <kisakathejones@gmail.com>
2025-08-15 15:30:59 +0300

    description section added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navigation from './components/Navigation'
 import ProfilePhoto from './components/ProfilePhoto'
 import Name from './components/Name'
 import Titles from './components/Titles'
+import Description from './components/Description'
 import Footer from './components/Footer'
 
 export default function Homepage() {
@@ -12,6 +13,7 @@ export default function Homepage() {
         <ProfilePhoto />
         <Name />
         <Titles />
+        <Description />
       </main>
       <Footer />
     </div>
