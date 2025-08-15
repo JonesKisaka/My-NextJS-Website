e5e5e08be99b5592f8f322658eddfaf35ce42f2f
JonesKisaka <kisakathejones@gmail.com>
2025-08-15 15:12:13 +0300

    photo size change

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -3,10 +3,10 @@ export default function Navigation() {
     <nav className="navigation">
       <div className="nav-container">
         <div className="nav-brand">
-          <a href="#home" className="nav-link">Jones</a>
+          <a href="#home" className="nav-link">Home</a>
         </div>
         <div className="nav-menu">
-          <a href="#codes" className="nav-link">Codes</a>
+          <a href="#about" className="nav-link">About</a>
         </div>
       </div>
     </nav>
